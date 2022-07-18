e7946977133fc234c8b4061e44e43a79df3fa29f
Sayak Mohanta <sayakmohanta1998@gmail.com>
2022-07-18 23:44:20 +0530

    APIs

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const { chats } = require("./data/data");
 
 const app = express();
+app.use(cors());
 dotenv.config();
 
 app.get("/", (req, res) => {
