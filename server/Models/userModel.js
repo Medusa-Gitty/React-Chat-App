4df965f8df71df84f41dfb0037e8b36324625c8f
Sayak Mohanta <sayakmohanta1998@gmail.com>
2022-07-19 16:09:31 +0530

    Database connection

--- server/Models/userModel.js
+++ server/Models/userModel.js
@@ -9,14 +9,14 @@ const userModel = mongoose.Schema(
     email: {
       type: String,
       required: true,
+      unique: true,
     },
     password: {
       type: String,
       required: true,
     },
-    profile: {
+    pic: {
       type: String,
-      required: true,
       default:
         "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
     },
---
+++ server/controllers/userController.js
@@ -0,0 +1,36 @@
+const User = require("../Models/userModel");
+
+exports.registerUser = async (req, res) => {
+  const { name, email, password, pic } = req.body;
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please enter all the required field");
+  }
+
+  const userExist = await User.findOne({ email });
+
+  if (userExist) {
+    res.status(400);
+    throw new Error("User already exist");
+  }
+
+  const user = await User.create({
+    name: name,
+    email: email,
+    password: password,
+    pic: pic,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Failed to create user");
+  }
+};
