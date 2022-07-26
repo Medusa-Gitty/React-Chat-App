a1f9b1f36aacbe8e9536b12798dc44047192bf32
Sayak Mohanta <sayakmohanta1998@gmail.com>
2022-07-26 23:36:42 +0530

    Chat Field Logics added

--- client/src/components/ChatPage/ChatterBox.jsx
+++ client/src/components/ChatPage/ChatterBox.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Box } from "@chakra-ui/react";
+import SingleChat from "./SingleChat";
 
 const ChatterBox = () => {
-  return <div>ChatterBox</div>;
+  //REDUX
+  const dispatch = useDispatch();
+  const { selectedChat } = useSelector((state) => state.chatData);
+
+  return (
+    <Box
+      d={{ base: selectedChat ? "flex" : "none", md: "flex" }}
+      alignItems="center"
+      flexDir="column"
+      p={3}
+      bg="white"
+      w={{ base: "100%", md: "68%" }}
+      borderRadius="lg"
+      borderWidth="1px"
+    >
+      <SingleChat />
+    </Box>
+  );
 };
 
 export default ChatterBox;
